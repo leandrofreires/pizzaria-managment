260108a9989a8473188470db3bee7b4d26b850b0
Leandro Freire <leandro.freire.work@gmail.com>
2019-06-06 08:15:49 -0300

    errors link Apololo hander add

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,26 @@
 import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloModule, APOLLO_OPTIONS, Apollo} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
 const uri = 'https://api.graph.cool/simple/v1/cjrtgxakk69ip0172ut8v5g53'; // <-- add the URL of the GraphQL server here
+const ErrorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ); }
+    ); }
+  if (networkError) { console.log(`[Network error]: ${networkError}`); }
+});
 export function createApollo(httpLink: HttpLink) {
   return {
-    link: httpLink.create({uri}),
+    link: ApolloLink.from([
+      ErrorLink,
+      httpLink.create({uri})
+    ]),
     cache: new InMemoryCache(),
   };
 }
